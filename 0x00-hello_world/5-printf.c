34cbd2faeb3eb4140b7c11983008fd0fb0459a86
omajugholee <omajugholee@gmail.com>
2022-09-09 03:31:15 +0100

    changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 /**
  * main - prints the string in the printf funtion
+ *
  * Description: using the min function
  * this program prints "with proper grammar, but the outcome is a piece of art"
  * return: 0
  */
 int main(void)
 {
-/** prints out the wanted string
+/**
+ * prints out the wanted string
  */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ * returns the output of 0
+ */
 	return (0);
 }
