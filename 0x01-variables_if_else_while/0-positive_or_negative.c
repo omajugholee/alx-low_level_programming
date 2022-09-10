e8d7f8baf919b999bf8b03df22829da8f16f9e7f
omajugholee <omajugholee@gmail.com>
2022-09-10 14:06:30 +0100

    pand n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n <0)
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	} else if (n == 0)
 	{ 
 		printf("%d is zero\n", n);
-	}else
+	} else
 	{
 		printf("%d is positive \n", n);
 
