e8cdf82c162a054ae9178bece75050cd0ea655d2
omajugholee <omajugholee@gmail.com>
2022-09-10 15:28:42 +0100

    corrects

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
-* main - print the sting in the put function 
-* 
-* Description: using main pur function
-* and print out programming is fun
-* Return: 0 
+ * main - Entry point
+ *
+ * Return: always 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
