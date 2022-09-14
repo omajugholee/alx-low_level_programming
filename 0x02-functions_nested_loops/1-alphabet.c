d7d8695d72d033f434fd611a91c8da187a1c2b57
omajugholee <omajugholee@gmail.com>
2022-09-14 02:40:50 +0100

    lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Destription - print the alphabelts in lowercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+
+	return (0);
+}
