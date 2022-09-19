f8c3966e77e72dd0b8270a0e8dc5917a96ef90ff
omajugholee <omajugholee@gmail.com>
2022-09-19 16:41:56 +0100

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * reset_to_98 : takes and return its value to 98
+ * reset_to_98 - takes and return its value to 98
  * @n : input int.
- * return: void
+ * return: always return void
  */
 void reset_to_98(int *n);
 {
-	int =
+	*n = 98;
+	return (0);
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,7 @@
-:x
-#
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
