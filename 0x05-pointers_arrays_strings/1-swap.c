721b34710116657b2eee28da3bc9a302c3d5c594
omajugholee <omajugholee@gmail.com>
2022-09-20 00:51:28 +0100

    swaping

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,15 @@
-#include <main.h>
+#include "main.h"
 /**
- * swa:x
- *
+ * swap_int - given two integers swap the varable
+ * @a: parameter 1
+ * @b: parameter 2
+ * retun- always return 0
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
 
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
