0897820c8e80e1a698ce35c9e24926ccaf84baf9
omajugholee <omajugholee@gmail.com>
2022-09-19 16:24:33 +0100

    statrt

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 : takes and return its value to 98
+ * @n : input int.
+ * return: void
+ */
+void reset_to_98(int *n);
+{
+	int =
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0. 98 Battery st.:
+1. Don't swap horses in crossing a stream
