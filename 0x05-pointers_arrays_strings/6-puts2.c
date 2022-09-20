c591da18243672d9a040ec81def358ef0369830c
omajugholee <omajugholee@gmail.com>
2022-09-21 00:09:45 +0100

    prints char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 -> funtion prints out other char of a string
+ * @str: paramenter for string
+ * return: returns void
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+		_putchar('\n')
+	}
+}
