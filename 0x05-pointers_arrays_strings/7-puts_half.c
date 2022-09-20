6f2046e81738a660e747ddd1e74382e0631b43fe
omajugholee <omajugholee@gmail.com>
2022-09-21 00:29:26 +0100

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,8 @@ void puts_half(char *str)
 	if (x % 2 == 1)
 		y = x / 2 + 1;
 	else
-		y = x / 2; 
-	for (i = y; i < x; i ++)
-		_purchar(str[i]);
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
