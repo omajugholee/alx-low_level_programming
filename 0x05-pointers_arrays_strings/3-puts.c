ba0e1bd9dd0f924b872589062772267de645c00f
omajugholee <omajugholee@gmail.com>
2022-09-20 18:27:56 +0100

    string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts -> a funtion that prints a string followed by a new line
+ * @str: a parameter that _puts
+ *return: returns void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; *str++)
+	{
+		_purchar(*str);
+	}
+	_putchar("\n");
+}
