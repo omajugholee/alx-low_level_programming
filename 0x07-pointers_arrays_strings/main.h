350ec2355229d274bf3097e8b6077fabe555fecc
omajugholee <omajugholee@gmail.com>
2022-09-27 07:33:20 +0100

    changing main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -9,5 +7,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to)
-#endif
+void set_string(char **s, char *to);
