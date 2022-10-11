aa2ab66f0f005eed8e02f766e435d0458f612f4b
omajugholee <omajugholee@gmail.com>
2022-10-11 15:03:22 +0100

    header dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,15 @@
  * @age: stores the dog age
  * @owner: stores the name of the owner
  */
- typedef struct dog dog_t
- struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+typedef struct dog dog_t
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
