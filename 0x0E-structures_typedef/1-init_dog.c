81244af2cd56fa8203304c299258f4d699c0b46f
omajugholee <omajugholee@gmail.com>
2022-10-11 15:19:10 +0100

    header dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,9 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog -> the function that initialiswes the varble type dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owers name:x
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
-#ifdef DOG_H
+#ifndef DOG_H
 #define DOG_H
 /**
- * struct dog-> the struture of our project
- * @name: stores the dog name
- * @age: stores the dog age
- * @owner: stores the name of the owner
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
@@ -13,13 +13,14 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t
-/*
- * dog_t -> function of struture dog
+/**
+ * dog_t - Typedef for struct dog
  */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
